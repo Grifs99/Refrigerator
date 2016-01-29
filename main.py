8f68396c45c9cf2a36750785bb2c249b95031d6a
Grifs99 <info.microwap@gmail.com>
2016-01-29 17:33:49 +0200

    Add youtube link detection and posting stats about it Add new python module isodate for youtube stuff to work

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import json
 import aiohttp
 import time
 import re
+from isodate import parse_duration
 
 starttime = int(time.time())
 logger = logging.getLogger('discord')
@@ -56,8 +57,8 @@ async def command_status():
 @client.command(name='u',
                 aliases=['urban', 'urbandic'],
                 help='Returns search term from urbandictionary.com')
-async def command_urbandictionary(*term):
-    search_term = '+'.join(term)
+async def command_urbandictionary(*, search_term):
+    # search_term = '+'.join(term)
     async with aiohttp.get('http://api.urbandictionary.com/v0/define?term={}'.format(search_term)) as resp:
         assert resp.status == 200
         resp_parsed = await resp.json()
@@ -69,7 +70,7 @@ async def command_urbandictionary(*term):
                 aliases=['user'],
                 help='Returns information about user',
                 pass_context=True)
-async def command_whois(ctx, username: str):
+async def command_whois(ctx, *, username: str):
     userinfo = discord.utils.get(ctx.message.server.members, name=username)
     userinfo_msg = 'Username: {}\n'.format(userinfo.name)
     userinfo_msg += 'Users ID: {}\n'.format(userinfo.id)
@@ -99,12 +100,13 @@ async def command_choose(*choices : str):
                 aliases=['playing'],
                 description='When Bot is bored!')
 @commands.has_permissions(manage_server=True)
-async def command_setgame(*gamename: str):
-    _gamename = ' '.join(gamename)
-    await client.change_status(game=discord.Game(name=_gamename))
+async def command_setgame(*, gamename: str):
+    await client.change_status(game=discord.Game(name=gamename))
 
 async def process_always(message):
     _twitch_re = re.compile(r'(.*:)//(twitch.tv|www.twitch.tv)(:[0-9]+)?(.*)', re.I)
+    _youtube_re = re.compile(r'(?:youtube.*?(?:v=|/v/)|youtu\.be/|yooouuutuuube.*?id=)([-_a-zA-Z0-9]+)', re.I)
+    _youtube_match = _youtube_re.search(message.content)
     _twitch_match = _twitch_re.match(message.content)
     if _twitch_match:
         channel = _twitch_match.group(4).split('#')[0].split(' ')[0].split('/')[1]
@@ -121,6 +123,61 @@ async def process_always(message):
                 await client.send_message(message.channel, str(resp_msg))
             elif not resp_parsed['stream']:
                 await client.send_message(message.channel, '**{}** channel at the moment is offline'.format(channel))
+    elif _youtube_match:
+        _id = _youtube_match.group(1)
+        async with aiohttp.get('https://www.googleapis.com/youtube/v3/videos?part=contentDetails%2C+snippet%2C+statistics&id={}&key={}'.format(_id, settings['youtube_api_key'])) as resp:
+            assert resp.status == 200
+            resp_parsed = await resp.json()
+            if resp_parsed.get('error'):
+                if resp_parsed['error']['code'] == 403:
+                    await client.send_message(message.channel, 'Failed to fetch content from youtube :(')
+                else:
+                    pass
+            items = resp_parsed['items'][0]
+            snippet = items['snippet']
+            statistics = items['statistics']
+            content_details = items['contentDetails']
+            title = snippet['title']
+            length = parse_duration(content_details['duration'])
+
+            intervals = (
+                ('w', 604800),  # 60 * 60 * 24 * 7
+                ('d', 86400),    # 60 * 60 * 24
+                ('h', 3600),    # 60 * 60
+                ('m', 60),
+                ('s', 1),
+                )
+
+            def display_time(seconds, granularity=2):
+                result = []
+
+                for name, count in intervals:
+                    value = seconds // count
+                    if value:
+                        seconds -= value * count
+                        result.append("{:.0f} {}".format(value, name))
+                return ' '.join(result[:granularity])
+
+            likes = float(statistics['likeCount'])
+            dislikes = float(statistics['dislikeCount'])
+            total_votes = likes + dislikes
+            total_percent = 100 * (likes / total_votes)
+            views = statistics['viewCount']
+            author = snippet['channelTitle']
+            upload_time = time.strptime(snippet['publishedAt'], "%Y-%m-%dT%H:%M:%S.000Z")
+            await client.send_message(message.channel, '**{}** – length **{}** – likes **{}**, dislikes **{}** (**{:.1f}%**) – **{}** views – **{}** on **{}**'.format(
+                title,
+                display_time(length.total_seconds(),4),
+                likes,
+                dislikes,
+                total_percent,
+                views,
+                author,
+                time.strftime("%Y.%m.%d", upload_time)
+            ))
+            channel = discord.utils.get(client.get_all_channels(), name='youtubestuff')
+            if (message.channel.id!=channel.id):
+                await client.send_message(channel, 'https://{}'.format(_youtube_match.group(0)))
 
 @client.command(name='info',
                 help='Returns information about Bot')
